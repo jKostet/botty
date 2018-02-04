9f5ace76fd69467c22394b6d10acb33fbd6e3b8c
jKostet <juho.kostet@helsinki.fi>
2018-02-04 20:27:56 +0200

    Add booting sounds and a lousy emergency stop (and then restart) button

--- botty.ino
+++ botty.ino
@@ -1,19 +1,64 @@
-
+// motor control pins
 int leftMotorForward = 3;
 int leftMotorBackward = 9;
 int rightMotorForward = 11;
 int rightMotorBackward = 10;
 
+int stopButton = 2;
+int speedDial = 15;
+
+int speaker = 14;
+
 int speed = 100;
 int state = 1;
 
 
-void setup() {
+void setup() { 
   // put your setup code here, to run once:
   pinMode(leftMotorForward, OUTPUT);
   pinMode(leftMotorBackward, OUTPUT);
   pinMode(rightMotorForward, OUTPUT);
   pinMode(rightMotorBackward, OUTPUT);
+  pinMode(stopButton, INPUT_PULLUP);
+  pinMode(speedDial, INPUT);
+  pinMode(speaker, OUTPUT);
+
+  tone(speaker, 400, 200);
+  delay(300);
+  tone(speaker, 400, 200);
+  delay(300);
+  tone(speaker, 400, 200);
+  delay(1000);
+  startMelody();
+  
+}
+
+
+
+void loop() {
+  listenStop();
+  dance();
+  
+}
+
+void listenStop() {
+  if (digitalRead(stopButton) == 0) {
+    // halt program for 10 seconds
+    tone(speaker, 800, 500);
+    delay(10*1000);
+
+    setup();
+    //startMelody();
+  }
+}
+
+void startMelody() {
+    tone(speaker, 400, 200);
+    delay(200);
+    tone(speaker, 500, 200);
+    delay(200);
+    tone(speaker, 600, 200);
+    delay(200);
 }
 
 void move(int time, int lF, int lB, int rF, int rB) {
@@ -24,13 +69,9 @@ void move(int time, int lF, int lB, int rF, int rB) {
   delay(time);
 }
 
-void loop() {
-  dance();
-  
-}
-
 void dance() {
-  // put your main code here, to run repeatedly:
+  listenStop();
+  
   state = random(1,5);
   switch(state) {
     case 1:
